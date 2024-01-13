3b55d26103ab08c97b624fe92767d7e4cbf951c0
Rodrigo <devrodrigolec@gmail.com>
2024-01-13 19:52:20 +0100

    quitado alert y agregado div alerta con setTimeOut para cuando ingresan un turno inválido

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const inputTurnoPersonalizado = document.getElementById(
   "turno-personalizado"
 ) as HTMLInputElement;
 const botonIngresarTurno = document.getElementById("ingresar-turno");
+const alertaDiv = document.getElementById('alerta');
 let turno: number = 0;
 
 const formatearPrefijo0 = (numero: number): string => {
@@ -34,12 +35,20 @@ const resetTurno = (): void => {
     numeroTurno.innerHTML = formatearPrefijo0(0);
 };
 
+const generarAlertaTurnoInválido = () => {
+  alertaDiv?.classList.remove('hidden');
+  setTimeout(() => {
+    alertaDiv?.classList.add('hidden');
+  },5000)
+}
+
 const ingresarTurnoPersonalizado = (): void => {
   /* Esto lo comento porque en una pantalla de turno normal, si se ingresa un turno personalizado es para llamar a alguien que perdió su turno y este ya pasó, pero luego el operario debe continuar con el turno que se quedó anteriormente. Por eso no igualo la cuenta del turno al turno personalizado, para que cuando el operario haga click en siguiente, continue donde se quedó. */
   /* turno = parseInt(inputTurnoPersonalizado.value); */
   if (parseInt(inputTurnoPersonalizado.value) < 0 || inputTurnoPersonalizado.value === '' ) {
-    alert("Ingrese un turno válido");
+    
     inputTurnoPersonalizado.value = "";
+    generarAlertaTurnoInválido()
     return;
   }
   if (numeroTurno !== undefined && numeroTurno !== null) {
