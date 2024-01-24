8e985209e197009f95cf2f0ca43f279470f1f867
Rodrigo <devrodrigolec@gmail.com>
2024-01-24 18:51:36 +0100

    corregido instanceof para elementos HTML

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ const numeroTurno = document.getElementById("numeroTurno");
 const botonSiguiente = document.getElementById("siguiente");
 const botonAnterior = document.getElementById("anterior");
 const botonReset = document.getElementById("reset");
-const inputTurnoPersonalizado = document.getElementById(
-  "turno-personalizado"
-) as HTMLInputElement;
+const inputTurnoPersonalizado = document.getElementById("turno-personalizado");
 const botonIngresarTurno = document.getElementById("ingresar-turno");
 const alertaDiv = document.getElementById("alerta");
 let turno: number = 0;
@@ -14,63 +12,82 @@ const formatearPrefijo0 = (numero: number): string => {
 };
 
 const turnoSiguiente = (): void => {
-  if (numeroTurno !== null && numeroTurno !== undefined)
+  if (numeroTurno && numeroTurno instanceof HTMLHeadingElement)
     numeroTurno.innerHTML = formatearPrefijo0(turno + 1);
   turno++;
 };
 
 const turnoAnterior = (): void => {
   if (turno === 0) return;
-  if (numeroTurno !== null && numeroTurno !== undefined)
+  if (numeroTurno && numeroTurno instanceof HTMLHeadingElement)
     numeroTurno.innerHTML = formatearPrefijo0(turno - 1);
   turno--;
 };
 
 const resetTurno = (): void => {
   turno = 0;
-  if (numeroTurno !== null && numeroTurno !== undefined)
+  if (numeroTurno && numeroTurno instanceof HTMLHeadingElement)
     numeroTurno.innerHTML = formatearPrefijo0(0);
 };
 
 const ingresarTurnoPersonalizado = (): void => {
   if (
-    parseInt(inputTurnoPersonalizado.value) < 0 ||
-    inputTurnoPersonalizado.value === ""
+    inputTurnoPersonalizado &&
+    inputTurnoPersonalizado instanceof HTMLInputElement
   ) {
-    inputTurnoPersonalizado.value = "";
-    alertaDiv?.classList.remove("hidden");
-    return;
+    if (
+      parseInt(inputTurnoPersonalizado.value) < 0 ||
+      inputTurnoPersonalizado.value === ""
+    ) {
+      inputTurnoPersonalizado.value = "";
+      if (alertaDiv && alertaDiv instanceof HTMLDivElement) {
+        alertaDiv.classList.remove("hidden");
+      }
+      return;
+    }
   }
-  if (numeroTurno !== undefined && numeroTurno !== null) {
+
+  if (
+    numeroTurno &&
+    numeroTurno instanceof HTMLHeadingElement &&
+    inputTurnoPersonalizado &&
+    inputTurnoPersonalizado instanceof HTMLInputElement
+  ) {
     numeroTurno.innerHTML = formatearPrefijo0(
       parseInt(inputTurnoPersonalizado.value)
     );
-    alertaDiv?.classList.add("hidden");
+    if (alertaDiv && alertaDiv instanceof HTMLDivElement) {
+      alertaDiv.classList.add("hidden");
+    }
     inputTurnoPersonalizado.value = "";
   }
 };
 
-if (numeroTurno !== undefined && numeroTurno !== null)
-  numeroTurno.innerHTML = formatearPrefijo0(turno);
+const iniciarApp = (): void => {
+  if (numeroTurno && numeroTurno instanceof HTMLHeadingElement)
+    numeroTurno.innerHTML = formatearPrefijo0(turno);
+};
 
-if (botonSiguiente !== null && botonSiguiente !== undefined) {
+if (botonSiguiente) {
   if (botonSiguiente instanceof HTMLButtonElement) {
     botonSiguiente.addEventListener("click", turnoSiguiente);
   }
 }
-if (botonAnterior !== null && botonAnterior !== undefined) {
+if (botonAnterior) {
   if (botonAnterior instanceof HTMLButtonElement) {
     botonAnterior.addEventListener("click", turnoAnterior);
   }
 }
-if (botonReset !== null && botonReset !== undefined) {
+if (botonReset) {
   if (botonReset instanceof HTMLButtonElement) {
     botonReset.addEventListener("click", resetTurno);
   }
 }
 
-if (botonIngresarTurno !== null && botonIngresarTurno !== undefined) {
+if (botonIngresarTurno) {
   if (botonIngresarTurno instanceof HTMLButtonElement) {
     botonIngresarTurno.addEventListener("click", ingresarTurnoPersonalizado);
   }
 }
+
+document.addEventListener("DOMContentLoaded", iniciarApp);
